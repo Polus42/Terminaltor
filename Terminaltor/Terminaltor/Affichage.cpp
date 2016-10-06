3c74170fdf687c3a00e5ab7b07f350d549b9659b
Polus42 <paulfigiel@laposte.net>
2016-10-06 15:36:05 +0200

    hud

--- Terminaltor/Terminaltor/Affichage.cpp
+++ Terminaltor/Terminaltor/Affichage.cpp
@@ -40,7 +40,10 @@ void Affichage::draw( Terrain& t )
 {
 	ReadConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
 		dwBufferCoord, &rcRegion);
+	
 	// Change buffer here
+	// Drawing paralax
+	int offset = 10;
 
 	int tile = -1;
 	for (int x = 0; x < t.Width(); x++)
@@ -57,7 +60,7 @@ void Affichage::draw( Terrain& t )
 			}
 		}
 	}
-
+	
 	drawHud(t);
 	WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize,
 		dwBufferCoord, &rcRegion);
@@ -77,7 +80,7 @@ void Affichage::drawHud(Terrain& t)
 	str = "LIFE : " + std::to_string(life);
 	for (int i = 0; i < str.length(); i++)
 	{
-		buffer[1][0].Char.UnicodeChar = str[i];
-		buffer[1][0].Attributes = 0x0005 | BACKGROUND_BLUE;
+		buffer[1][t.Width()+i-str.length()-1].Char.UnicodeChar = str[i];
+		buffer[1][t.Width() + i-str.length()-1].Attributes = 0x0005 | BACKGROUND_BLUE;
 	}
 }
